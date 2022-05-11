7255f3e17695c9fc3c9bbea64da36d5880b66ff1
Jeff Tian <jeff.tian@outlook.com>
2022-05-11 16:01:20 +0800

    Update flutter.rb
    Use cn mirror

--- Casks/flutter.rb
+++ Casks/flutter.rb
@@ -2,7 +2,7 @@ cask "flutter" do
   version "1.2.1"
   sha256 '74ac8397ea29720f116980ea00cf60c34430be1f64489b407f7cf95553babbef'
 
-  url "https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_v#{version}-stable.zip"
+  url "https://storage.flutter-io.cn/flutter_infra_release/releases/stable/macos/flutter_macos_v#{version}-stable.zip"
   name "flutter"
   homepage "https://flutter.io"
 
